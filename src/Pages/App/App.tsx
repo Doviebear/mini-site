50251300fda93895ca8eaefa1b33e98dc544b738
dshalev <dshalev@umich.edu>
2024-07-23 12:28:54 -0400

    starting tower

---
+++ src/Pages/App/App.tsx
@@ -0,0 +1,46 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="scene">
+        <div className="tower-position">
+          <div className="tower">
+            <div className="tower-left tower-face-side"></div>
+            <div className="tower-right tower-face-side"></div>
+            <div className="tower-top tower-face-top"></div>
+          </div>
+        </div>
+        <div className="stud-position-1">
+          <div className="stud">
+            <div className="stud-top"></div>
+            <div className="stud-side"></div>
+            <div className="stud-bottom"></div>
+          </div>
+        </div>
+        <div className="stud-position-2">
+          <div className="stud">
+            <div className="stud-top"></div>
+            <div className="stud-side"></div>
+            <div className="stud-bottom"></div>
+          </div>
+        </div>
+        <div className="stud-position-3">
+          <div className="stud">
+            <div className="stud-top"></div>
+            <div className="stud-side"></div>
+            <div className="stud-bottom"></div>
+          </div>
+        </div>
+        <div className="stud-position-4">
+          <div className="stud">
+            <div className="stud-top"></div>
+            <div className="stud-side"></div>
+            <div className="stud-bottom"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
